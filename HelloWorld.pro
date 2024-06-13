7234b157059e2eb14303e73b12a6942e5c4a9e32
nabilweq <nabeeltkanr@gmail.com>
2024-06-13 19:49:57 +0530

    updated the design

--- HelloWorld.pro
+++ HelloWorld.pro
@@ -1,15 +1,13 @@
 QT += core gui network widgets
 CONFIG += c++11
 
-TARGET = HelloWorld
+TARGET = OdooPOS
 TEMPLATE = app
 
 SOURCES += main.cpp \
            productcard.cpp
 
 HEADERS += productcard.h
-
-RESOURCES += resources.qrc
 
 INCLUDEPATH += /opt/homebrew/opt/qt/include
 LIBS += -F/opt/homebrew/opt/qt/lib -framework QtCore -framework QtGui -framework QtWidgets
--- productcard.cpp
+++ productcard.cpp
@@ -1,23 +1,34 @@
 #include "productcard.h"
 #include <QPixmap>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QDebug>
 
 ProductCard::ProductCard(const QString &productName, const QString &productPrice, const QString &productImage, QWidget *parent)
-    : QWidget(parent) {
+    : QFrame(parent), manager(new QNetworkAccessManager(this)) {
+    setFrameShape(QFrame::StyledPanel);
+    setFrameShadow(QFrame::Raised);
+    setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;");
+
     QVBoxLayout *layout = new QVBoxLayout(this);
 
     imageLabel = new QLabel(this);
-    QPixmap pixmap(productImage);
-    imageLabel->setPixmap(pixmap.scaled(100, 100, Qt::KeepAspectRatio));
+    imageLabel->setAlignment(Qt::AlignCenter);
+    imageLabel->setFixedSize(150, 150);
+    layout->addWidget(imageLabel);
 
     nameLabel = new QLabel(productName, this);
-    nameLabel->setStyleSheet("color: black;"); // Set text color to black
-    priceLabel = new QLabel(productPrice, this);
-    priceLabel->setStyleSheet("color: black;"); // Set text color to black
-    addButton = new QPushButton("Add to Cart", this);
-
-    layout->addWidget(imageLabel);
+    nameLabel->setStyleSheet("font-size: 16px; font-weight: bold; color: black;");
+    nameLabel->setAlignment(Qt::AlignCenter);
     layout->addWidget(nameLabel);
+
+    priceLabel = new QLabel(productPrice, this);
+    priceLabel->setStyleSheet("font-size: 14px; color: green;");
+    priceLabel->setAlignment(Qt::AlignCenter);
     layout->addWidget(priceLabel);
+
+    addButton = new QPushButton("Add to Cart", this);
+    addButton->setStyleSheet("background-color: orange; color: white; padding: 5px;");
     layout->addWidget(addButton);
 
     setLayout(layout);
@@ -25,4 +36,24 @@ ProductCard::ProductCard(const QString &productName, const QString &productPrice
     connect(addButton, &QPushButton::clicked, this, [=]() {
         emit addToCart(productName, productPrice);
     });
+
+    connect(manager, &QNetworkAccessManager::finished, this, &ProductCard::onImageDownloaded);
+
+    QUrl imageUrl(productImage);
+    qDebug() << "Requesting image from URL:" << imageUrl.toString();
+    QNetworkRequest request(imageUrl);
+    manager->get(request);
+}
+
+void ProductCard::onImageDownloaded(QNetworkReply *reply) {
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray imageData = reply->readAll();
+        QPixmap pixmap;
+        pixmap.loadFromData(imageData);
+        imageLabel->setPixmap(pixmap.scaled(imageLabel->size(), Qt::KeepAspectRatio));
+    } else {
+        qWarning() << "Image download error: " << reply->errorString();
+        imageLabel->setText("Image not found");
+    }
+    reply->deleteLater();
 }
