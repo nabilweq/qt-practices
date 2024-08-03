e6fb7d2fb2e4f3782c03fb710d20ef79d99ad6c9
nabilweq <nabeeltkanr@gmail.com>
2024-08-03 22:55:51 +0530

    ui enhancements

--- productcard.cpp
+++ productcard.cpp
@@ -18,7 +18,7 @@ ProductCard::ProductCard(const QString &productName, const QString &productPrice
     layout->addWidget(imageLabel);
 
     nameLabel = new QLabel(productName, this);
-    nameLabel->setStyleSheet("font-size: 12px; font-weight: bold; color: grey;");
+    nameLabel->setStyleSheet("font-size: 14px; font-weight: bold; color: grey;");
     nameLabel->setAlignment(Qt::AlignCenter);
     layout->addWidget(nameLabel);
 
